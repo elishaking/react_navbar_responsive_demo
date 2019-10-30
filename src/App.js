fc3f523ef1c0593304552af40a96c2c0e32e33da
King Elisha <ek.chibueze@gmail.com>
2019-10-30 20:01:21 +0100

    new features

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ import Navbar from 'react_navbar_responsive';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar
+        links={[
+          {
+            to: "/home",
+            text: "Home"
+          },
+          {
+            html: (<li><a href="/about">About</a></li>)
+          },
+          {
+            to: "/contact",
+            text: "Contact"
+          }
+        ]} />
     </div>
   );
 }
